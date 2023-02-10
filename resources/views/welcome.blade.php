a290f55c9bf4a12e3848afc29d974d09f254e420
Jose. Palazuelos <punksolid@gmail.com>
2023-02-10 15:05:48 -0700

    escenario cuando no hay prospectos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -274,9 +274,44 @@ plugins: [
             </div>
 
             <!-- Destacado del mes -->
+            @if($prospect)
+                <div class="bg-gradient-to-r from-teal-500 to-cyan-600 pb-16 lg:relative lg:z-10 lg:pb-0">
+                    <div class="relative isolate overflow-hidden py-24 px-6 sm:py-32 lg:px-8">
+                        <img src="{{ $prospect->image_url }}" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1097 845" aria-hidden="true" class="hidden transform-gpu blur-3xl sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:w-[68.5625rem]">
+                            <path fill="url(#10724532-9d81-43d2-bb94-866e98dd6e42)" fill-opacity=".2" d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z" />
+                            <defs>
+                                <linearGradient id="10724532-9d81-43d2-bb94-866e98dd6e42" x1="1097.04" x2="-141.165" y1=".22" y2="363.075" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="#776FFF" />
+                                    <stop offset="1" stop-color="#FF4694" />
+                                </linearGradient>
+                            </defs>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1097 845" aria-hidden="true" class="absolute left-1/2 -top-52 -z-10 w-[68.5625rem] -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0">
+                            <path fill="url(#8ddc7edb-8983-4cd7-bccb-79ad21097d70)" fill-opacity=".2" d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z" />
+                            <defs>
+                                <linearGradient id="8ddc7edb-8983-4cd7-bccb-79ad21097d70" x1="1097.04" x2="-141.165" y1=".22" y2="363.075" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="#776FFF" />
+                                    <stop offset="1" stop-color="#FF4694" />
+                                </linearGradient>
+                            </defs>
+                        </svg>
+                        <div class="mx-auto max-w-2xl text-center">
+                            <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Destacado Del Mes</h2>
+                            <p class="mt-6 text-lg leading-8 text-gray-200 font-bold">{{ $prospect->name }}</p>
+                            <p class="mt-6 text-lg leading-8 text-gray-200 font-semibold">{{ $prospect->description }}</p>
+                        </div>
+                        <div class="mx-auto max-w-2xl text-center">
+                            {{--                        button --}}
+                            <a href="{{ $prospect->facebook_link }}"><x-primary-button>Página de Facebook</x-primary-button></a>
+                            <a href="{{ $prospect->google_maps_link }}"><x-primary-button>Google Maps</x-primary-button></a>
+                        </div>
+
+                    </div>
+                </div>
+            @else
             <div class="bg-gradient-to-r from-teal-500 to-cyan-600 pb-16 lg:relative lg:z-10 lg:pb-0">
                 <div class="relative isolate overflow-hidden py-24 px-6 sm:py-32 lg:px-8">
-                    <img src="{{ $prospect->image_url }}" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1097 845" aria-hidden="true" class="hidden transform-gpu blur-3xl sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:w-[68.5625rem]">
                         <path fill="url(#10724532-9d81-43d2-bb94-866e98dd6e42)" fill-opacity=".2" d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z" />
                         <defs>
@@ -296,18 +331,12 @@ plugins: [
                         </defs>
                     </svg>
                     <div class="mx-auto max-w-2xl text-center">
-                        <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Destacado Del Mes</h2>
-                        <p class="mt-6 text-lg leading-8 text-gray-200 font-bold">{{ $prospect->name }}</p>
-                        <p class="mt-6 text-lg leading-8 text-gray-200 font-semibold">{{ $prospect->description }}</p>
+                        <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">No Hay Destacado Del Mes Aún</h2>
+                        <p class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Se el primero en registrar un negocio.</p>
                     </div>
-                    <div class="mx-auto max-w-2xl text-center">
-{{--                        button --}}
-                        <a href="{{ $prospect->facebook_link }}"><x-primary-button>Página de Facebook</x-primary-button></a>
-                        <a href="{{ $prospect->google_maps_link }}"><x-primary-button>Google Maps</x-primary-button></a>
-                    </div>
-
                 </div>
             </div>
+            @endif
 
         </main>
         <footer class="bg-gray-50" aria-labelledby="footer-heading">
