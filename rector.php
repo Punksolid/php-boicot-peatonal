ab46c2b115b32c06fba0eea9c8f1172594f03249
Jose. Palazuelos <punksolid@gmail.com>
2023-02-16 15:35:04 -0700

    deleted ProspectPolicy.php

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodeQuality\Rector\ClassMethod\ReturnTypeFromStrictScalarReturnExprRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
