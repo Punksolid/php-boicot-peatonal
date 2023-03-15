aeb0eb6368dde89cdef8a63de838c325aa56103f
Jose. Palazuelos <punksolid@gmail.com>
2023-03-15 17:59:14 -0600

    Fix image for featured prospect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -278,7 +278,7 @@ plugins: [
             @if($prospect)
                 <div class="bg-gradient-to-r from-teal-500 to-cyan-600 pb-16 lg:relative lg:z-10 lg:pb-0">
                     <div class="relative isolate overflow-hidden py-24 px-6 sm:py-32 lg:px-8">
-                        <img src="{{ $prospect->image_url }}" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
+                        <img src="{{ $prospect->getFirstMediaUrl() }}" alt="Imagen de {{ $prospect->name }} con banqueta invadida." class="absolute inset-0 -z-10 h-full w-full object-cover">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1097 845" aria-hidden="true" class="hidden transform-gpu blur-3xl sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:w-[68.5625rem]">
                             <path fill="url(#10724532-9d81-43d2-bb94-866e98dd6e42)" fill-opacity=".2" d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z" />
                             <defs>
