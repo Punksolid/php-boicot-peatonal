8baeaea93eca8d5344088105426fa453f885479a
Jose. Palazuelos <punksolid@gmail.com>
2023-04-27 22:19:32 -0600

    Agregada inteligencia artificial

--- app/Mail/FeaturedProspectOfTheMonth.php
+++ app/Mail/FeaturedProspectOfTheMonth.php
@@ -8,18 +8,47 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use OpenAI\Laravel\Facades\OpenAI;
 
 class FeaturedProspectOfTheMonth extends Mailable
 {
     use Queueable, SerializesModels;
+
+    public string $title;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct(public $prospect)
+    public function __construct(public $prospect, public $chatGptLetter = null)
     {
+        logger()->info('Enviando Email');
+        $instructions = <<<TEXT
+Formula un email, que invite a la persona a ir a google maps o pagina en facebook y calificar con una estrella a la empresa {$this->prospect->name},
+El objetivo es desincentivar a los negocios que invaden el espacio público que es de todos.
+Firma la IA de BoicotPeatonal.ORG
+Escribelo de forma amigable y hasta graciosa.Si puedes aprovecha para criticar a las autoridades.
+TEXT;
+
+
+        $responseContent = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                ['role' => 'user', 'content' => $instructions],
+            ],
+        ]);
+
+        $responseTitle = OpenAI::chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                ['role' => 'user', 'content' => $instructions],
+                ['role' => 'user', 'content' => 'Escribe el titulo para el email'],
+            ],
+        ]);
+
+        $this->chatGptLetter = $responseContent->choices[0]->message->content;
+        $this->title = $responseTitle->choices[0]->message->content;
     }
 
     /**
@@ -30,7 +59,7 @@ class FeaturedProspectOfTheMonth extends Mailable
     public function envelope()
     {
         return new Envelope(
-            subject: 'Una invitación a hacer una diferencia',
+            subject: $this->title,
         );
     }
 
