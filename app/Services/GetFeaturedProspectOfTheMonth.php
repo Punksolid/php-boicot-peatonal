ec7cea21c9b2179ff0af73a9d15bc4a38035e9c0
Jose. Palazuelos <punksolid@gmail.com>
2023-05-02 22:13:42 -0600

    fixed select new featured prospect of the month

--- app/Services/GetFeaturedProspectOfTheMonth.php
+++ app/Services/GetFeaturedProspectOfTheMonth.php
@@ -3,18 +3,21 @@
 namespace App\Services;
 
 use App\Models\Prospect;
+use App\Models\User;
+use LaravelQuadraticVoting\Services\QuadraticVoteService;
 
 class GetFeaturedProspectOfTheMonth
 {
 
     public function __invoke(): ?Prospect
     {
-        $getFeaturedProspectOfTheMonth = $this->getFeaturedProspectOfTheMonth();
+        $getFeaturedProspectOfTheMonth = $this->getNewFeaturedProspectOfTheMonth();
 
         if (!$getFeaturedProspectOfTheMonth) {
             $newFeaturedProspect = $this->selectNewFeaturedProspect();
 
-            if (!$newFeaturedProspect instanceof \App\Models\Prospect) {
+
+            if (!$newFeaturedProspect instanceof Prospect) {
                 return null;
             }
             $newFeaturedProspect->featured_at = now();
@@ -30,16 +33,26 @@ class GetFeaturedProspectOfTheMonth
     private function selectNewFeaturedProspect(): ?Prospect
     {
         return Prospect::
-        notFeatured()
-            ->whereHas('voters')
-            ->with('voters.ideas')
+            notFeatured()
+                ->whereHas('voters')
+                ->with('voters.ideas')
+                ->withSum('voters', 'votes.quantity')
+                ->orderByDesc('voters_sum_votesquantity')
+                ->first();
+
+    }
+
+    private function getNewFeaturedProspectOfTheMonth(): Prospect
+    {
+        $newMostVotedProspect = Prospect::with('voters')
             ->withSum('voters', 'votes.quantity')
             ->orderByDesc('voters_sum_votesquantity')
             ->first();
 
+        return $newMostVotedProspect;
     }
 
-    private function getFeaturedProspectOfTheMonth()
+    private function getFeaturedProspectOfTheMonth(): Prospect
     {
         return Prospect::orderBy('featured_at', 'desc')->first();
     }
